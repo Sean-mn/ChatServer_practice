c1ec641c002d860373fcea1fa8a4966c0988223c
Seanyee1227 <b01080080329@gmail.com>
2025-04-06 01:57:10 +0900

    Chat 스크립트 구현

--- ChatServer_Practice/Assets/Scripts/Chat.cs
+++ ChatServer_Practice/Assets/Scripts/Chat.cs
@@ -4,9 +4,36 @@ using UnityEngine.UI;
 
 public class Chat : MonoBehaviour
 {
-    private static Chat _intstance;
-    public static Chat Instance { get { return _intstance; } }
+    private static Chat _instance;
+    public static Chat Instance { get { return _instance; } }
 
     [SerializeField] private InputField _sendInput;
     [SerializeField] private RectTransform _chatContext;
+    [SerializeField] private ScrollRect _chatScrollRect;
+    [SerializeField] private Text _chatText;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void ShowMessage(string msg)
+    {
+        _chatText.text += _chatText.text == "" ? msg : "\n" + msg;
+
+        Fit(_chatContext.GetComponent<RectTransform>());
+        Fit(_chatContext);
+
+        Invoke("ScrollDelay", 0.03f);
+    }
+
+    private void Fit(RectTransform rect)
+    {
+        LayoutRebuilder.ForceRebuildLayoutImmediate(rect);
+    }
+
+    private void ScrollDelay()
+    {
+        _chatScrollRect.verticalScrollbar.value = 0;
+    }
 }
