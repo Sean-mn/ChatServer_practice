d73b1d43e46ed4b177a0c52d9c9d84f53a861b7b
Seanyee1227 <b01080080329@gmail.com>
2025-04-06 03:07:28 +0900

    ScreenManager 수정

--- ChatServer_Practice/Assets/Scripts/ScreenManager.cs
+++ ChatServer_Practice/Assets/Scripts/ScreenManager.cs
@@ -1,13 +1,11 @@
 ﻿using UnityEngine;
 
 /// <summary>
-/// 
 /// class : SetScreenSize
 /// desc  : 해상도 및 화면 모드 초기 설정
 /// 
 /// function:
 /// SetScreenSize(int width, int height, FullScreenMode screenMode): 화면 크기 설정 및 모드 설정
-/// 
 /// </summary>
 public class ScreenManager : MonoBehaviour
 {
